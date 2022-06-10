2c7f69f7badbe692d607382e92d32aa11bd6b01c
Self <bhubaneswar.behera@gmail.com>
2022-06-10 08:04:34 +0530

    Updated with Filed Injectioin

--- 03-Bill-Payment-App/src/in/ashokit/Main.java
+++ 03-Bill-Payment-App/src/in/ashokit/Main.java
@@ -1,16 +1,39 @@
 package in.ashokit;
 
+import java.lang.reflect.Field;
+
+
+
 public class Main {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws Exception {
+		//Setter Injection Starts###################################
 		BillCollector billCollector = new BillCollector();
 		//Injecting CreditCard object into BillCollector through Setter method (Setter Injection)
 		billCollector.setPayment(new CrediCardPayment());
-		billCollector.collectPayment(1400);
+		billCollector.collectPayment(1400.00);
+		//Setter Injection End###################################
 		
+		//Constructor Injection Starts###################################
 		//Injecting DebitCard object into BillCollector through Constructor (Constructor Injection)
 		BillCollector billCollector1 = new BillCollector(new DebitCardPayment());
-		billCollector1.collectPayment(1500);
+		billCollector1.collectPayment(1500.00);
+		//Constructor Injection Ends###################################
+		
+		
+		//Field Injection Starts###################################
+		Class<?> cls = Class.forName("in.ashokit.BillCollector");
+		
+		Field field = cls.getDeclaredField("payment");
+		field.setAccessible(true);
+		
+		Object object = cls.newInstance();
+		field.set(object, new UpiPayment());
+		
+		BillCollector billCollector3 = (BillCollector)object;
+		billCollector3.collectPayment(2000.00);
+		//Field Injection Ends###################################
+		
 	}
 
 }
