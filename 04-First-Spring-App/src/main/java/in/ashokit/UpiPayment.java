8051d9295f10b68d4f344a0552f800a7f0bc8810
Self <bhubaneswar.behera@gmail.com>
2022-06-17 08:05:56 +0530

    Updated

--- 04-First-Spring-App/src/main/java/in/ashokit/UpiPayment.java
+++ 04-First-Spring-App/src/main/java/in/ashokit/UpiPayment.java
@@ -1,6 +1,10 @@
 package in.ashokit;
 
 public class UpiPayment implements IPayment {
+	
+	public UpiPayment () {
+		System.out.println("UpiPayment constructor called");
+	}
 
 	public String pay(double amount) {
 		return "Payment successful through UpiPayment";
