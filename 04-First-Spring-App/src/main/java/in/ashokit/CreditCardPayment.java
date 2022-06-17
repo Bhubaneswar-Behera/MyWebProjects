fdb6fb6d7165ffa8a210623abb40f01ff22b7d41
Self <bhubaneswar.behera@gmail.com>
2022-06-17 08:06:43 +0530

    Updated

--- 04-First-Spring-App/src/main/java/in/ashokit/CreditCardPayment.java
+++ 04-First-Spring-App/src/main/java/in/ashokit/CreditCardPayment.java
@@ -1,6 +1,10 @@
 package in.ashokit;
 
 public class CreditCardPayment implements IPayment {
+	
+	public CreditCardPayment() {
+		System.out.println("CreditCard constructor called");
+	}
 
 	public String pay(double amount) {
 		return "Payment successful through Creditcard";
--- 04-First-Spring-App/src/main/java/in/ashokit/DebitCardPayment.java
+++ 04-First-Spring-App/src/main/java/in/ashokit/DebitCardPayment.java
@@ -1,6 +1,10 @@
 package in.ashokit;
 
 public class DebitCardPayment implements IPayment {
+	
+	public DebitCardPayment() {
+		System.out.println("DebitCardPayment constcuctor called");
+	}
 
 	public String pay(double amount) {
 		return "Payment successful through DebitCard";
