8b9dc17a950724aac2f3e448b692d6433bff9a0e
Self <bhubaneswar.behera@gmail.com>
2022-06-24 11:03:14 +0530

    Updated

---
+++ 10-SpringBoot-CopmonentScan/src/main/java/in/ashokit/config/KafkaConfig.java
@@ -0,0 +1,16 @@
+package in.ashokit.config;
+
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * 
+ * @author Bhubaneswar Behera @date 24-Jun-2022
+ *
+ */
+@Configuration
+public class KafkaConfig {
+	public KafkaConfig() {
+		System.out.println("KafkaConfig :: Constructor");
+	}
+
+}
