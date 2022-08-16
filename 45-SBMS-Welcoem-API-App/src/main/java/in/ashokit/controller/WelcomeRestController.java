0edcba885c48d932193df12b53587456d696ca7c
Self <bhubaneswar.behera@gmail.com>
2022-08-16 07:06:47 +0530

    Updated

--- 45-SBMS-Welcoem-API-App/src/main/java/in/ashokit/controller/WelcomeRestController.java
+++ 45-SBMS-Welcoem-API-App/src/main/java/in/ashokit/controller/WelcomeRestController.java
@@ -1,5 +1,7 @@
 package in.ashokit.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -11,9 +13,13 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class WelcomeRestController {
 	
+	@Autowired
+	private Environment environment;
+	
 	@GetMapping("/welcome")
 	public String getWelcomeMessage() {
-		String message = "Welcome To Spring Boot and Microservice...!!!";
+		String property = environment.getProperty("server.port");
+		String message = "Welcome To Spring Boot and Microservice...!!!" + " Port is :: " + property;
 		
 		return message;
 		
